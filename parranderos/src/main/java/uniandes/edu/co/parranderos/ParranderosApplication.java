9501f7f9f0ce54202417d9d6dfe3e2900bdf858c
usuariocoursera1 <jp.reyes39@uniandes.edu.co>
2023-08-14 15:32:39 -0500

    Final tarea Construcción de controladores

--- parranderos/src/main/java/uniandes/edu/co/parranderos/ParranderosApplication.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/ParranderosApplication.java
@@ -3,7 +3,6 @@ package uniandes.edu.co.parranderos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ParranderosApplication{
 
---
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/BaresController.java
@@ -0,0 +1,68 @@
+package uniandes.edu.co.parranderos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import uniandes.edu.co.parranderos.modelo.Bar;
+import uniandes.edu.co.parranderos.repositorio.BarRepository;
+
+import org.springframework.ui.Model;
+
+
+//@Controller
+@RestController
+public class BaresController {
+
+    @Autowired
+    private BarRepository barRepository;
+
+    @GetMapping("/bares")
+    public String bares(Model model) {
+        model.addAttribute("bares", barRepository.darBares());
+        //return "bares";
+        return model.toString();
+
+}
+
+    @GetMapping("/bares/new")
+    public String barForm(Model model) {
+        model.addAttribute("bar", new Bar());
+        return "barNuevo";
+    }
+
+    @PostMapping("/bares/new/save")
+    public String barGuardar(@ModelAttribute Bar bar) {
+        barRepository.insertarBar(bar.getNombre(), bar.getCiudad(), bar.getPresupuesto(), bar.getCant_sedes());
+        return "redirect:/bares";
+    }
+
+    @GetMapping("/bares/{id}/edit")
+    public String barEditarForm(@PathVariable("id") int id, Model model) {
+        Bar bar = barRepository.darBar(id);
+        if (bar != null) {
+            model.addAttribute("bar", bar);
+            return "barEditar";
+        } else {
+            return "redirect:/bares";
+        }
+    }
+
+    @PostMapping("/bares/{id}/edit/save")
+    public String barEditarGuardar(@PathVariable("id") long id, @ModelAttribute Bar bar) {
+        barRepository.actualizarBar(((long) id), bar.getNombre(), bar.getCiudad(), bar.getPresupuesto(),
+                bar.getCant_sedes());
+        return "redirect:/bares";
+    }
+
+    @GetMapping("/bares/{id}/delete")
+    public String barEliminar(@PathVariable("id") long id) {
+        barRepository.eliminarBar(id);
+        return "redirect:/bares";
+    }
+
+}
